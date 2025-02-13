61cc18683344cce949ff03ba8832f85b8207a4ea
sunaga-kazunori <sunaga-kazunori@mitsue.co.jp>
2025-02-13 20:30:15 +0900

    chore: セミコロンを付けるルールに変更

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,5 +5,13 @@ module.exports = {
     'plugin:react/jsx-runtime',
     '@electron-toolkit/eslint-config-ts/recommended',
     '@electron-toolkit/eslint-config-prettier'
-  ]
-}
+  ],
+  rules: {
+    semi: ['error', 'always'],
+    'semi-spacing': ['error', { after: true, before: false }],
+    'semi-style': ['error', 'last'],
+    'no-extra-semi': 'error',
+    'no-unexpected-multiline': 'error',
+    'no-unreachable': 'error'
+  }
+};
