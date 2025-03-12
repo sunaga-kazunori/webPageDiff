0e53d4831893a4c47030704aa629444bf4a3adc4
sunaga-kazunori <sunaga-kazunori@mitsue.co.jp>
2025-03-12 21:05:40 +0900

    test: BasicAuthenticationInput テストを追加

---
+++ src/renderer/src/__test__/components/BasicAuthenticationInput.test.tsx
@@ -0,0 +1,35 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { useState } from 'react';
+import { describe, expect, it, vi } from 'vitest';
+import BasicAuthenticationInput from '../../components/BasicAuthenticationInput';
+
+describe('BasicAuthenticationInput', () => {
+  it('入力が行われた時、setText が正しく呼び出される', () => {
+    const setTextMock = vi.fn();
+    render(<BasicAuthenticationInput label="ユーザー名" value="" setText={setTextMock} />);
+
+    const inputElement = screen.getByRole('textbox');
+
+    fireEvent.change(inputElement, { target: { value: 'newUsername' } });
+
+    expect(setTextMock).toHaveBeenCalledTimes(1);
+    expect(setTextMock).toHaveBeenCalledWith('newUsername');
+  });
+
+  it('入力された値が value に反映される', () => {
+    // 親コンポーネントの模擬テスト
+    const ParentComponent: React.FC = () => {
+      const [text, setText] = useState('');
+
+      return <BasicAuthenticationInput label="ユーザー名" value={text} setText={setText} />;
+    };
+
+    render(<ParentComponent />);
+
+    const inputElement = screen.getByRole<HTMLInputElement>('textbox');
+
+    expect(inputElement.value).toBe('');
+    fireEvent.change(inputElement, { target: { value: 'newUser' } });
+    expect(inputElement.value).toBe('newUser');
+  });
+});
