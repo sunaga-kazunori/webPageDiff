cef1370d0a39ed80538a077dfa3ec7a778bfd53d
sunaga-kazunori <sunaga-kazunori@mitsue.co.jp>
2025-03-19 13:10:16 +0900

    fix: スタイル調整

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -103,7 +103,7 @@ function App(): JSX.Element {
         {...(isLoading ? { inert: 'true' } : {})}
       >
         <div
-          className={`p-5 relative after:absolute after:top-0 after:right-0 after:bottom-0 after:left-0 after:pointer-events-none after:content-[''] after:bg-black after:opacity-0 after:transition-opacity after:duration-200
+          className={`p-5 relative after:absolute after:top-0 after:right-0 after:bottom-0 after:left-0 after:pointer-events-none after:content-[''] after:bg-black after:opacity-0 after:transition-opacity after:duration-200 overflow-y-auto
         ${hasCheckedDiff ? 'after:pointer-events-auto after:opacity-70' : ''}`}
         >
           <Tabs activeIndex={activeTabIndex} setActiveIndex={setActiveTabIndex}>
@@ -127,7 +127,7 @@ function App(): JSX.Element {
             viewPortSize={viewPortSize}
             setViewPortSize={setViewPortSize}
           ></ViewPortSizeInput>
-          <div className="mt-7 text-center text-lg font-bold">
+          <div className="mt-10 text-center text-lg font-bold">
             <Button handleClick={handleCheckedDiffClick} label="差分確認"></Button>
           </div>
           {hasCheckedDiff && (
@@ -138,7 +138,7 @@ function App(): JSX.Element {
             ></Button>
           )}
         </div>
-        <div className="bg-gray-50 p-4">
+        <div className="bg-gray-50 p-4 overflow-y-auto">
           <ErrorMessage diffPixelList={diffPixelList} diffImageList={diffImageList}></ErrorMessage>
           <Table
             sourceUrlList={sourceUrlState.urlList}
