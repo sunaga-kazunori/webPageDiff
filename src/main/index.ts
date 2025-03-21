05a6b7291c701f1b36a673d8c37b87c9c78b1d4e
sunaga-kazunori <sunaga-kazunori@mitsue.co.jp>
2025-03-21 13:10:33 +0900

    docs: コメント整理

--- src/main/index.ts
+++ src/main/index.ts
@@ -15,7 +15,6 @@ type DiffPixelList = (string | number)[];
 let mainWindow: null | BrowserWindow = null;
 
 function createWindow(): void {
-  // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 1200,
     height: 720,
@@ -37,8 +36,8 @@ function createWindow(): void {
     return { action: 'deny' };
   });
 
-  // HMR for renderer base on electron-vite cli.
-  // Load the remote URL for development or the local html file for production.
+  // electron-vite cliを基にしたレンダラのHMR
+  // 開発中はリモートURLを読み込み、本番環境ではローカルのhtmlファイルを読み込む
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
     mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL']);
   } else {
@@ -46,31 +45,20 @@ function createWindow(): void {
   }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+// このメソッドはElectronが初期化を終了し、ブラウザウィンドウを作成する準備ができたときに呼び出される
 app.whenReady().then(() => {
-  // Set app user model id for windows
+  // Windows向けにアプリのユーザーモデルIDを設定
   electronApp.setAppUserModelId('com.electron');
 
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
+  // 開発中はF12でデベロッパーツールを開閉でき、本番環境ではCommandOrControl + Rを無視する。
+  // 詳細は https://github.com/alex8088/electron-toolkit/tree/master/packages/utils を参照
   app.on('browser-window-created', (_, window) => {
     optimizer.watchWindowShortcuts(window);
   });
 
-  // IPC test
-  ipcMain.on('ping', () => console.log('pong'));
-
   createWindow();
 
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
-
+  // ユーザーが入力した情報を元に差分量の確認・差分画像の作成を行う
   ipcMain.on(
     'sendAdvanceData',
     async (
@@ -199,6 +187,7 @@ app.whenReady().then(() => {
     }
   );
 
+  // 画像保存のダイアログを表示
   ipcMain.on('saveImage', async (_, imageData, index) => {
     const { filePath } = await dialog.showSaveDialog({
       title: '画像を保存',
@@ -223,14 +212,9 @@ app.whenReady().then(() => {
   });
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+// すべてのウィンドウが閉じられたときに終了する
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
-
-// In this file you can include the rest of your app"s specific main process
-// code. You can also put them in separate files and require them here.
