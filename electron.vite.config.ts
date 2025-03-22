2394500bb067280d4b83d644fe4f298712b6124b
sunaga-kazunori <sunaga-kazunori@mitsue.co.jp>
2025-03-23 02:18:16 +0900

    fix: @tailwindcss/viteの型情報が読み込まれないため型チェックを無視

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,7 +1,10 @@
-import { resolve } from 'path';
-import { defineConfig, externalizeDepsPlugin } from 'electron-vite';
-import react from '@vitejs/plugin-react';
+// @tailwindcss/viteの型情報が読み込まれないため型チェックを無視
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   main: {
